7c1d0360194fd1730eeb76bd4670d9467f0ae27e
elio <elio@netfilmx.com>
2018-05-27 21:40:59 -0300

    v8

---
+++ js/main.js
@@ -0,0 +1,67 @@
+'use strict';
+
+function openEditor(imgId) {
+    /*
+    var clickedImg = gImgs.find(function (img) {
+        return imgId === img.id;
+    });
+    */
+    document.querySelector('.back-to-imgs').style.display = 'initial';
+    document.querySelector('.img-gallery').style.display = 'none';
+    document.querySelector('.headings').style.display = 'none';
+    document.querySelector('.logo').style.display = 'none';
+    document.querySelector('#down-arrow').style.display = 'none';
+    document.querySelector('.editor').style.display = 'flex';
+    document.querySelector('footer').style.position = 'relative';
+    document.querySelector('body').style.padding = '0px 0px 0px';
+
+    // #ToDo: clean this!
+    var up = document.getElementById('upload'),
+    canvas = document.getElementById('canvas'),
+    uploaded = document.getElementById('meme')  ;
+
+    up.addEventListener('click', uploadToImgur);
+
+    function uploadToImgur() {
+        var img;
+        try {
+            img = canvas.toDataURL('image/gif', 1.0).split(',')[1];
+        } catch(e) {
+            img = canvas.toDataURL().split(',')[1];
+        }
+        $.ajax({
+            url: 'https://api.imgur.com/3/upload.json',
+            type: 'POST',
+            data: {
+                type: 'base64',
+                name: 'meme.png',
+                title: 'Title',
+                description: 'Description',
+                image: img
+            },
+            headers: {
+                Authorization: 'Client-ID 4b64f0eff077008'
+            },
+            dataType: 'json'
+        }).success(function(data) {
+            var l = data.data.link;
+            uploaded.value = l;
+        }).error(function(err) {
+            console.error(err);
+        });
+
+    }
+
+}
+
+function closeEditor() {
+    document.querySelector('#overlay-exit').style.display = 'none';
+    document.querySelector('.back-to-imgs').style.display = 'none';    
+    document.querySelector('.img-gallery').style.display = 'block';
+    document.querySelector('.headings').style.display = 'block';
+    document.querySelector('.logo').style.display = 'block';
+    document.querySelector('#down-arrow').style.display = 'block';
+    document.querySelector('.editor').style.display = 'none';
+    document.querySelector('footer').style.position = 'fixed';
+    document.querySelector('body').style.padding = '0px 0px 45px';
+}
---
+++ js/script.js
@@ -0,0 +1,65 @@
+angular.module('Meme',[])
+.controller('MainController',function($scope) {
+	$scope.ruggeris = [
+		{ id: 0, url: './img/ruggeri/ruggeri-1.jpg'},
+		{ id: 1, url: './img/ruggeri/ruggeri-2.jpg'},
+		{ id: 2, url: './img/ruggeri/ruggeri-3.jpg'},
+		{ id: 3, url: './img/ruggeri/ruggeri-4.jpg'},
+		{ id: 4, url: './img/ruggeri/ruggeri-5.jpg'},
+		{ id: 5, url: './img/ruggeri/ruggeri-6.jpg'},
+		{ id: 6, url: './img/ruggeri/ruggeri-7.jpg'},
+		{ id: 7, url: './img/ruggeri/ruggeri-8.jpg'},
+		{ id: 8, url: './img/ruggeri/ruggeri-9.jpg'},
+		{ id: 9, url: './img/ruggeri/ruggeri-10.jpg'},
+		{ id: 10, url: './img/ruggeri/ruggeri-11.jpg'},
+		{ id: 11, url: './img/ruggeri/ruggeri-12.jpg'},
+		{ id: 12, url: './img/ruggeri/ruggeri-13.jpg'},
+		{ id: 13, url: './img/ruggeri/ruggeri-14.jpg'},		
+	  ];
+	/*
+	var ruggeriMeme;
+
+	function selectedRuggeri(ruggeriId) {
+		ruggeriMeme = ruggeriId;
+		console.meme('','',ruggeriMeme) ;
+	}
+	*/
+
+	$scope.toptext = '';
+	$scope.bottomtext = '';
+	$scope.selectedMeme = 'ruggeri1';
+	$scope.memes = [];
+
+	for(var key in console.list) {
+		$scope.memes.push({name:key,url:console.list[key]});
+	}
+	
+	$scope.alterMeme = function(){
+		var url = $scope.memes[$scope.memes.indexOf($scope.selectedMeme)].name;
+		console.meme($scope.toptext,$scope.bottomtext,url);
+	}
+
+	$scope.create = function() {
+		var uri = document.getElementById('canvas').toDataURL();
+		downloadImage(uri,generateName());
+	}
+
+	function generateName() {
+		var n = [];
+		for(var i =0; i < 10;i++) {
+			n.push((Math.floor(Math.random() *16)).toString(16));
+		}
+		return n.join('');
+	};
+
+	function downloadImage(uri,name) {
+		var link = document.createElement('a');
+		link.download = name;
+		link.href = uri;
+		link.click();
+	};
+
+})
+.run(function() {
+	console.meme('','','ruggeri1');
+});
